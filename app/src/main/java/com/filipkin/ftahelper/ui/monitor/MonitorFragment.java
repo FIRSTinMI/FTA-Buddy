0501aff22b7afd968b0cfb4bf8647b9c75dee09b
filip <me@filipkin.com>
2023-10-14 13:54:41 -0400

    Event code persists

--- app/src/main/java/com/filipkin/ftahelper/ui/monitor/MonitorFragment.java
+++ app/src/main/java/com/filipkin/ftahelper/ui/monitor/MonitorFragment.java
@@ -1,22 +1,17 @@
 package com.filipkin.ftahelper.ui.monitor;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Looper;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -30,15 +25,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Objects;
 
 public class MonitorFragment extends Fragment {
 
     private FragmentMonitorBinding binding;
     private URI uri;
-    private FieldState field = new FieldState() {};
+    private final FieldState field = new FieldState() {};
     private MonitorViewModel monitorViewModel;
+    private boolean firstConnection = true;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         monitorViewModel = new ViewModelProvider(this).get(MonitorViewModel.class);
@@ -46,15 +41,22 @@ public class MonitorFragment extends Fragment {
         binding = FragmentMonitorBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        SharedPreferences eventCode = Objects.requireNonNull(getContext()).getSharedPreferences("eventCode", 0);
+        String savedEvent = eventCode.getString("eventCode", null);
+        if (savedEvent != null) {
+            binding.monitorEvent.setText(savedEvent);
+            parseEventCodeAndConnect(savedEvent);
+        }
+
+        // Change in event code or ip field
         binding.monitorEvent.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 String eventInput = binding.monitorEvent.getText().toString();
 
-                if (eventInput.matches("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$")) {
-                    uri = URI.create("ws://" + eventInput + ":8284/");
-                    System.out.println(uri.toString());
-                    openWebSocket();
-                }
+                eventCode.edit().putString("eventCode", eventInput).apply();
+
+                firstConnection = true;
+                parseEventCodeAndConnect(eventInput);
 
                 binding.monitorEvent.clearFocus();
             }
@@ -177,6 +179,14 @@ public class MonitorFragment extends Fragment {
         return root;
     }
 
+    private void parseEventCodeAndConnect(String eventInput) {
+        if (eventInput.matches("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$")) {
+            uri = URI.create("ws://" + eventInput + ":8284/");
+            System.out.println(uri.toString());
+            openWebSocket();
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -188,108 +198,99 @@ public class MonitorFragment extends Fragment {
             @Override
             public void onTextReceived(String s) {
                 final String message = s;
-                Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.i("WebSocket", message);
-                        try {
-                            JSONObject jObject = new JSONObject(message);
-                            field.field = jObject.getInt("field");
-                            field.match = jObject.getInt("match");
-                            field.time = jObject.getString("time");
+                Objects.requireNonNull(getActivity()).runOnUiThread(() -> {
+                    Log.i("WebSocket", message);
+                    try {
+                        JSONObject jObject = new JSONObject(message);
+                        field.field = jObject.getInt("field");
+                        field.match = jObject.getInt("match");
+                        field.time = jObject.getString("time");
 
-                            JSONObject blue1 = jObject.getJSONObject("blue1");
-                            field.blue1.number = blue1.getInt("number");
-                            field.blue1.ds = blue1.getInt("ds");
-                            field.blue1.radio = blue1.getInt("radio");
-                            field.blue1.rio = blue1.getInt("rio");
-                            field.blue1.code = blue1.getInt("code");
-                            field.blue1.bwu = blue1.getDouble("bwu");
-                            field.blue1.battery = blue1.getDouble("battery");
-                            field.blue1.ping = blue1.getInt("ping");
-                            field.blue1.packets = blue1.getInt("packets");
+                        JSONObject blue1 = jObject.getJSONObject("blue1");
+                        field.blue1.number = blue1.getInt("number");
+                        field.blue1.ds = blue1.getInt("ds");
+                        field.blue1.radio = blue1.getInt("radio");
+                        field.blue1.rio = blue1.getInt("rio");
+                        field.blue1.code = blue1.getInt("code");
+                        field.blue1.bwu = blue1.getDouble("bwu");
+                        field.blue1.battery = blue1.getDouble("battery");
+                        field.blue1.ping = blue1.getInt("ping");
+                        field.blue1.packets = blue1.getInt("packets");
 
-                            JSONObject blue2 = jObject.getJSONObject("blue2");
-                            field.blue2.number = blue2.getInt("number");
-                            field.blue2.ds = blue2.getInt("ds");
-                            field.blue2.radio = blue2.getInt("radio");
-                            field.blue2.rio = blue2.getInt("rio");
-                            field.blue2.code = blue2.getInt("code");
-                            field.blue2.bwu = blue2.getDouble("bwu");
-                            field.blue2.battery = blue2.getDouble("battery");
-                            field.blue2.ping = blue2.getInt("ping");
-                            field.blue2.packets = blue2.getInt("packets");
+                        JSONObject blue2 = jObject.getJSONObject("blue2");
+                        field.blue2.number = blue2.getInt("number");
+                        field.blue2.ds = blue2.getInt("ds");
+                        field.blue2.radio = blue2.getInt("radio");
+                        field.blue2.rio = blue2.getInt("rio");
+                        field.blue2.code = blue2.getInt("code");
+                        field.blue2.bwu = blue2.getDouble("bwu");
+                        field.blue2.battery = blue2.getDouble("battery");
+                        field.blue2.ping = blue2.getInt("ping");
+                        field.blue2.packets = blue2.getInt("packets");
 
-                            JSONObject blue3 = jObject.getJSONObject("blue3");
-                            field.blue3.number = blue3.getInt("number");
-                            field.blue3.ds = blue3.getInt("ds");
-                            field.blue3.radio = blue3.getInt("radio");
-                            field.blue3.rio = blue3.getInt("rio");
-                            field.blue3.code = blue3.getInt("code");
-                            field.blue3.bwu = blue3.getDouble("bwu");
-                            field.blue3.battery = blue3.getDouble("battery");
-                            field.blue3.ping = blue3.getInt("ping");
-                            field.blue3.packets = blue3.getInt("packets");
+                        JSONObject blue3 = jObject.getJSONObject("blue3");
+                        field.blue3.number = blue3.getInt("number");
+                        field.blue3.ds = blue3.getInt("ds");
+                        field.blue3.radio = blue3.getInt("radio");
+                        field.blue3.rio = blue3.getInt("rio");
+                        field.blue3.code = blue3.getInt("code");
+                        field.blue3.bwu = blue3.getDouble("bwu");
+                        field.blue3.battery = blue3.getDouble("battery");
+                        field.blue3.ping = blue3.getInt("ping");
+                        field.blue3.packets = blue3.getInt("packets");
 
-                            JSONObject red1 = jObject.getJSONObject("red1");
-                            field.red1.number = red1.getInt("number");
-                            field.red1.ds = red1.getInt("ds");
-                            field.red1.radio = red1.getInt("radio");
-                            field.red1.rio = red1.getInt("rio");
-                            field.red1.code = red1.getInt("code");
-                            field.red1.bwu = red1.getDouble("bwu");
-                            field.red1.battery = red1.getDouble("battery");
-                            field.red1.ping = red1.getInt("ping");
-                            field.red1.packets = red1.getInt("packets");
+                        JSONObject red1 = jObject.getJSONObject("red1");
+                        field.red1.number = red1.getInt("number");
+                        field.red1.ds = red1.getInt("ds");
+                        field.red1.radio = red1.getInt("radio");
+                        field.red1.rio = red1.getInt("rio");
+                        field.red1.code = red1.getInt("code");
+                        field.red1.bwu = red1.getDouble("bwu");
+                        field.red1.battery = red1.getDouble("battery");
+                        field.red1.ping = red1.getInt("ping");
+                        field.red1.packets = red1.getInt("packets");
 
-                            JSONObject red2 = jObject.getJSONObject("red2");
-                            field.red2.number = red2.getInt("number");
-                            field.red2.ds = red2.getInt("ds");
-                            field.red2.radio = red2.getInt("radio");
-                            field.red2.rio = red2.getInt("rio");
-                            field.red2.code = red2.getInt("code");
-                            field.red2.bwu = red2.getDouble("bwu");
-                            field.red2.battery = red2.getDouble("battery");
-                            field.red2.ping = red2.getInt("ping");
-                            field.red2.packets = red2.getInt("packets");
+                        JSONObject red2 = jObject.getJSONObject("red2");
+                        field.red2.number = red2.getInt("number");
+                        field.red2.ds = red2.getInt("ds");
+                        field.red2.radio = red2.getInt("radio");
+                        field.red2.rio = red2.getInt("rio");
+                        field.red2.code = red2.getInt("code");
+                        field.red2.bwu = red2.getDouble("bwu");
+                        field.red2.battery = red2.getDouble("battery");
+                        field.red2.ping = red2.getInt("ping");
+                        field.red2.packets = red2.getInt("packets");
 
-                            JSONObject red3 = jObject.getJSONObject("red3");
-                            field.red3.number = red3.getInt("number");
-                            field.red3.ds = red3.getInt("ds");
-                            field.red3.radio = red3.getInt("radio");
-                            field.red3.rio = red3.getInt("rio");
-                            field.red3.code = red3.getInt("code");
-                            field.red3.bwu = red3.getDouble("bwu");
-                            field.red3.battery = red3.getDouble("battery");
-                            field.red3.ping = red3.getInt("ping");
-                            field.red3.packets = red3.getInt("packets");
+                        JSONObject red3 = jObject.getJSONObject("red3");
+                        field.red3.number = red3.getInt("number");
+                        field.red3.ds = red3.getInt("ds");
+                        field.red3.radio = red3.getInt("radio");
+                        field.red3.rio = red3.getInt("rio");
+                        field.red3.code = red3.getInt("code");
+                        field.red3.bwu = red3.getDouble("bwu");
+                        field.red3.battery = red3.getDouble("battery");
+                        field.red3.ping = red3.getInt("ping");
+                        field.red3.packets = red3.getInt("packets");
 
-                            monitorViewModel.getField().setValue(field);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
+                        monitorViewModel.getField().setValue(field);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
+
                 });
             }
 
             @Override
             public void onOpen() {
-                Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(getContext(), "Connected to websocket", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                if (!firstConnection) return;
+                firstConnection = false;
+                Objects.requireNonNull(getActivity()).runOnUiThread(() -> Toast.makeText(getContext(), "Connected to websocket", Toast.LENGTH_SHORT).show());
             }
 
             @Override
             public void onException(Exception e) {
-                System.out.println(e);
-                Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(getContext(), "Error connecting " + e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
+                System.out.println(e.getMessage());
+                Objects.requireNonNull(getActivity()).runOnUiThread(() -> Toast.makeText(getContext(), "Error connecting " + e.getMessage(), Toast.LENGTH_LONG).show());
             }
         };
     }

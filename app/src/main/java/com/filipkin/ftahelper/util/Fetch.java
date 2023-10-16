ccb80baaa34b462216329df6a4b005dd6d4f5b44
filip <me@filipkin.com>
2023-10-15 21:40:12 -0400

    Fetch post

--- app/src/main/java/com/filipkin/ftahelper/util/Fetch.java
+++ app/src/main/java/com/filipkin/ftahelper/util/Fetch.java
@@ -2,8 +2,10 @@ package com.filipkin.ftahelper.util;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class Fetch {
     private static OkHttpClient client = new OkHttpClient();
@@ -11,6 +13,16 @@ public class Fetch {
     public static Call get(String url, Callback callback) {
         Request request = new Request.Builder()
                 .url(url)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+    public static Call post(String url, String body, Callback callback) {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(body, MediaType.parse("application/json; charset=utf-8")))
                 .build();
         Call call = client.newCall(request);
         call.enqueue(callback);

bbdab1744643798ecd29fa04f9273276891a3126
filip <me@filipkin.com>
2023-10-31 01:08:48 -0400

    Notifications of some sort?

--- app/src/main/java/com/filipkin/ftahelper/ui/monitor/MonitorFragment.java
+++ app/src/main/java/com/filipkin/ftahelper/ui/monitor/MonitorFragment.java
@@ -22,6 +22,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.filipkin.ftahelper.R;
 import com.filipkin.ftahelper.util.Fetch;
+import com.filipkin.ftahelper.util.Notifications;
 import com.filipkin.ftahelper.util.WebSocket;
 import com.filipkin.ftahelper.databinding.FragmentMonitorBinding;
 
@@ -46,12 +47,14 @@ public class MonitorFragment extends Fragment {
     private boolean firstConnection = true;
     private URI last_connection_uri;
     private WebSocket ws;
+    FieldState field = new FieldState() {};
+    private View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         monitorViewModel = new ViewModelProvider(this).get(MonitorViewModel.class);
 
         binding = FragmentMonitorBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        root = binding.getRoot();
 
         SharedPreferences sharedPreferences = requireContext().getSharedPreferences("FTABuddy", 0);
 
@@ -310,7 +313,6 @@ public class MonitorFragment extends Fragment {
         if (last_connection_uri != null && last_connection_uri.equals(uri)) return;
         Log.i("Websocket", "Connection initializing to " + uri.toString());
         firstConnection = true;
-        FieldState field = new FieldState() {};
 
         // Close open websocket
         if (ws != null && ws.open) ws.close();
@@ -318,137 +320,8 @@ public class MonitorFragment extends Fragment {
             int failedConnections = 0;
             @Override
             public void onTextReceived(String s) {
-                final String message = s;
-                requireActivity().runOnUiThread(() -> {
-                    Log.i("WebSocket", message);
-                    try {
-                        JSONObject jObject = new JSONObject(message);
-                        if (!jObject.getString("type").equals("monitorUpdate")) return;
-                        field.field = jObject.getInt("field");
-                        field.match = jObject.getInt("match");
-                        field.time = jObject.getString("time");
-
-                        JSONObject blue1 = jObject.getJSONObject("blue1");
-                        JSONObject blue2 = jObject.getJSONObject("blue2");
-                        JSONObject blue3 = jObject.getJSONObject("blue3");
-                        JSONObject red1 = jObject.getJSONObject("red1");
-                        JSONObject red2 = jObject.getJSONObject("red2");
-                        JSONObject red3 = jObject.getJSONObject("red3");
-
-                        requireContext();
-                        Vibrator v = (Vibrator) requireActivity().getSystemService(Context.VIBRATOR_SERVICE);
-
-                        // Vibrate on lost connection if match in progress
-                        if (field.field < 4 && field.field > 0) {
-                            if ((field.blue1.ds == 1 && field.blue1.ds > blue1.getInt("ds")) ||
-                                    (field.blue2.ds == 1 && field.blue2.ds > blue2.getInt("ds")) ||
-                                    (field.blue3.ds == 1 && field.blue3.ds > blue3.getInt("ds")) ||
-                                    (field.red1.ds == 1 && field.red1.ds > red1.getInt("ds")) ||
-                                    (field.red2.ds == 1 && field.red2.ds > red2.getInt("ds")) ||
-                                    (field.red3.ds == 1 && field.red3.ds > red3.getInt("ds"))) {
-                                v.vibrate(VibrationEffect.createWaveform(new long[]{1000, 200, 300, 200, 300}, new int[]{255, 0, 255, 0, 255}, -1));
-                                Log.i("Monitor", "Lost Driverstation Ethernet");
-                            } else if ((field.blue1.ds == 1 && field.blue1.ds < blue1.getInt("ds")) ||
-                                    (field.blue2.ds == 1 && field.blue2.ds < blue2.getInt("ds")) ||
-                                    (field.blue3.ds == 1 && field.blue3.ds < blue3.getInt("ds")) ||
-                                    (field.red1.ds == 1 && field.red1.ds < red1.getInt("ds")) ||
-                                    (field.red2.ds == 1 && field.red2.ds < red2.getInt("ds")) ||
-                                    (field.red3.ds == 1 && field.red3.ds < red3.getInt("ds"))) {
-                                v.vibrate(VibrationEffect.createWaveform(new long[]{300, 200, 300, 200, 300}, new int[]{255, 0, 255, 0, 255}, -1));
-                                Log.i("Monitor", "Lost Driverstation");
-                            } else if (field.blue1.radio > blue1.getInt("radio") ||
-                                    field.blue2.radio > blue2.getInt("radio") ||
-                                    field.blue3.radio > blue3.getInt("radio") ||
-                                    field.red1.radio > red1.getInt("radio") ||
-                                    field.red2.radio > red2.getInt("radio") ||
-                                    field.red3.radio > red3.getInt("radio")) {
-                                v.vibrate(VibrationEffect.createWaveform(new long[]{300, 200, 300}, new int[]{255, 0, 255}, -1));
-                                Log.i("Monitor", "Lost Radio");
-                            } else if (field.blue1.rio > blue1.getInt("rio") ||
-                                    field.blue2.rio > blue2.getInt("rio") ||
-                                    field.blue3.rio > blue3.getInt("rio") ||
-                                    field.red1.rio > red1.getInt("rio") ||
-                                    field.red2.rio > red2.getInt("rio") ||
-                                    field.red3.rio > red3.getInt("rio")) {
-                                v.vibrate(VibrationEffect.createOneShot(500, 255));
-                                Log.i("Monitor", "Lost Rio");
-                            } else if (field.blue1.code > blue1.getInt("code") ||
-                                    field.blue2.code > blue2.getInt("code") ||
-                                    field.blue3.code > blue3.getInt("code") ||
-                                    field.red1.code > red1.getInt("code") ||
-                                    field.red2.code > red2.getInt("code") ||
-                                    field.red3.code > red3.getInt("code")) {
-                                v.vibrate(VibrationEffect.createOneShot(200, 255));
-                                Log.i("Monitor", "Lost Code");
-                            }
-                        }
-
-                        field.blue1.number = blue1.getInt("number");
-                        field.blue1.ds = blue1.getInt("ds");
-                        field.blue1.radio = blue1.getInt("radio");
-                        field.blue1.rio = blue1.getInt("rio");
-                        field.blue1.code = blue1.getInt("code");
-                        field.blue1.bwu = blue1.getDouble("bwu");
-                        field.blue1.battery = blue1.getDouble("battery");
-                        field.blue1.ping = blue1.getInt("ping");
-                        field.blue1.packets = blue1.getInt("packets");
-
-                        field.blue2.number = blue2.getInt("number");
-                        field.blue2.ds = blue2.getInt("ds");
-                        field.blue2.radio = blue2.getInt("radio");
-                        field.blue2.rio = blue2.getInt("rio");
-                        field.blue2.code = blue2.getInt("code");
-                        field.blue2.bwu = blue2.getDouble("bwu");
-                        field.blue2.battery = blue2.getDouble("battery");
-                        field.blue2.ping = blue2.getInt("ping");
-                        field.blue2.packets = blue2.getInt("packets");
-
-                        field.blue3.number = blue3.getInt("number");
-                        field.blue3.ds = blue3.getInt("ds");
-                        field.blue3.radio = blue3.getInt("radio");
-                        field.blue3.rio = blue3.getInt("rio");
-                        field.blue3.code = blue3.getInt("code");
-                        field.blue3.bwu = blue3.getDouble("bwu");
-                        field.blue3.battery = blue3.getDouble("battery");
-                        field.blue3.ping = blue3.getInt("ping");
-                        field.blue3.packets = blue3.getInt("packets");
-
-                        field.red1.number = red1.getInt("number");
-                        field.red1.ds = red1.getInt("ds");
-                        field.red1.radio = red1.getInt("radio");
-                        field.red1.rio = red1.getInt("rio");
-                        field.red1.code = red1.getInt("code");
-                        field.red1.bwu = red1.getDouble("bwu");
-                        field.red1.battery = red1.getDouble("battery");
-                        field.red1.ping = red1.getInt("ping");
-                        field.red1.packets = red1.getInt("packets");
-
-                        field.red2.number = red2.getInt("number");
-                        field.red2.ds = red2.getInt("ds");
-                        field.red2.radio = red2.getInt("radio");
-                        field.red2.rio = red2.getInt("rio");
-                        field.red2.code = red2.getInt("code");
-                        field.red2.bwu = red2.getDouble("bwu");
-                        field.red2.battery = red2.getDouble("battery");
-                        field.red2.ping = red2.getInt("ping");
-                        field.red2.packets = red2.getInt("packets");
-
-                        field.red3.number = red3.getInt("number");
-                        field.red3.ds = red3.getInt("ds");
-                        field.red3.radio = red3.getInt("radio");
-                        field.red3.rio = red3.getInt("rio");
-                        field.red3.code = red3.getInt("code");
-                        field.red3.bwu = red3.getDouble("bwu");
-                        field.red3.battery = red3.getDouble("battery");
-                        field.red3.ping = red3.getInt("ping");
-                        field.red3.packets = red3.getInt("packets");
-
-                        monitorViewModel.getField().setValue(field);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                });
+                Log.i("WebSocket", s);
+                handleFieldMonitorUpdate(s);
             }
 
             @Override
@@ -488,5 +361,133 @@ public class MonitorFragment extends Fragment {
                 last_connection_uri = null;
             }
         };
+    }
+
+    private void handleFieldMonitorUpdate(String message) {
+        try {
+            JSONObject jObject = new JSONObject(message);
+            if (!jObject.getString("type").equals("monitorUpdate")) return;
+            field.field = jObject.getInt("field");
+            field.match = jObject.getInt("match");
+            field.time = jObject.getString("time");
+
+            JSONObject blue1 = jObject.getJSONObject("blue1");
+            JSONObject blue2 = jObject.getJSONObject("blue2");
+            JSONObject blue3 = jObject.getJSONObject("blue3");
+            JSONObject red1 = jObject.getJSONObject("red1");
+            JSONObject red2 = jObject.getJSONObject("red2");
+            JSONObject red3 = jObject.getJSONObject("red3");
+
+            requireContext();
+            Vibrator v = (Vibrator) requireActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
+            // Vibrate on lost connection if match in progress
+            if (field.field < 4 && field.field > 0) {
+                if ((field.blue1.ds == 1 && field.blue1.ds > blue1.getInt("ds")) ||
+                        (field.blue2.ds == 1 && field.blue2.ds > blue2.getInt("ds")) ||
+                        (field.blue3.ds == 1 && field.blue3.ds > blue3.getInt("ds")) ||
+                        (field.red1.ds == 1 && field.red1.ds > red1.getInt("ds")) ||
+                        (field.red2.ds == 1 && field.red2.ds > red2.getInt("ds")) ||
+                        (field.red3.ds == 1 && field.red3.ds > red3.getInt("ds"))) {
+                    Notifications.makeStatusNotification(true,"Lost DS Ethernet", requireContext());
+                    Log.i("Monitor", "Lost Driverstation Ethernet");
+                } else if ((field.blue1.ds == 1 && field.blue1.ds < blue1.getInt("ds")) ||
+                        (field.blue2.ds == 1 && field.blue2.ds < blue2.getInt("ds")) ||
+                        (field.blue3.ds == 1 && field.blue3.ds < blue3.getInt("ds")) ||
+                        (field.red1.ds == 1 && field.red1.ds < red1.getInt("ds")) ||
+                        (field.red2.ds == 1 && field.red2.ds < red2.getInt("ds")) ||
+                        (field.red3.ds == 1 && field.red3.ds < red3.getInt("ds"))) {
+                    Notifications.makeStatusNotification(true,"Lost DS", requireContext());
+                    Log.i("Monitor", "Lost Driverstation");
+                } else if (field.blue1.radio > blue1.getInt("radio") ||
+                        field.blue2.radio > blue2.getInt("radio") ||
+                        field.blue3.radio > blue3.getInt("radio") ||
+                        field.red1.radio > red1.getInt("radio") ||
+                        field.red2.radio > red2.getInt("radio") ||
+                        field.red3.radio > red3.getInt("radio")) {
+                    Notifications.makeStatusNotification(false,"Lost Radio", requireContext());
+                    Log.i("Monitor", "Lost Radio");
+                } else if (field.blue1.rio > blue1.getInt("rio") ||
+                        field.blue2.rio > blue2.getInt("rio") ||
+                        field.blue3.rio > blue3.getInt("rio") ||
+                        field.red1.rio > red1.getInt("rio") ||
+                        field.red2.rio > red2.getInt("rio") ||
+                        field.red3.rio > red3.getInt("rio")) {
+                    Notifications.makeStatusNotification(false,"Lost RIO", requireContext());
+                } else if (field.blue1.code > blue1.getInt("code") ||
+                        field.blue2.code > blue2.getInt("code") ||
+                        field.blue3.code > blue3.getInt("code") ||
+                        field.red1.code > red1.getInt("code") ||
+                        field.red2.code > red2.getInt("code") ||
+                        field.red3.code > red3.getInt("code")) {
+                    Notifications.makeStatusNotification(false,"Lost Code", requireContext());
+                    Log.i("Monitor", "Lost Code");
+                }
+            }
+
+            field.blue1.number = blue1.getInt("number");
+            field.blue1.ds = blue1.getInt("ds");
+            field.blue1.radio = blue1.getInt("radio");
+            field.blue1.rio = blue1.getInt("rio");
+            field.blue1.code = blue1.getInt("code");
+            field.blue1.bwu = blue1.getDouble("bwu");
+            field.blue1.battery = blue1.getDouble("battery");
+            field.blue1.ping = blue1.getInt("ping");
+            field.blue1.packets = blue1.getInt("packets");
+
+            field.blue2.number = blue2.getInt("number");
+            field.blue2.ds = blue2.getInt("ds");
+            field.blue2.radio = blue2.getInt("radio");
+            field.blue2.rio = blue2.getInt("rio");
+            field.blue2.code = blue2.getInt("code");
+            field.blue2.bwu = blue2.getDouble("bwu");
+            field.blue2.battery = blue2.getDouble("battery");
+            field.blue2.ping = blue2.getInt("ping");
+            field.blue2.packets = blue2.getInt("packets");
+
+            field.blue3.number = blue3.getInt("number");
+            field.blue3.ds = blue3.getInt("ds");
+            field.blue3.radio = blue3.getInt("radio");
+            field.blue3.rio = blue3.getInt("rio");
+            field.blue3.code = blue3.getInt("code");
+            field.blue3.bwu = blue3.getDouble("bwu");
+            field.blue3.battery = blue3.getDouble("battery");
+            field.blue3.ping = blue3.getInt("ping");
+            field.blue3.packets = blue3.getInt("packets");
+
+            field.red1.number = red1.getInt("number");
+            field.red1.ds = red1.getInt("ds");
+            field.red1.radio = red1.getInt("radio");
+            field.red1.rio = red1.getInt("rio");
+            field.red1.code = red1.getInt("code");
+            field.red1.bwu = red1.getDouble("bwu");
+            field.red1.battery = red1.getDouble("battery");
+            field.red1.ping = red1.getInt("ping");
+            field.red1.packets = red1.getInt("packets");
+
+            field.red2.number = red2.getInt("number");
+            field.red2.ds = red2.getInt("ds");
+            field.red2.radio = red2.getInt("radio");
+            field.red2.rio = red2.getInt("rio");
+            field.red2.code = red2.getInt("code");
+            field.red2.bwu = red2.getDouble("bwu");
+            field.red2.battery = red2.getDouble("battery");
+            field.red2.ping = red2.getInt("ping");
+            field.red2.packets = red2.getInt("packets");
+
+            field.red3.number = red3.getInt("number");
+            field.red3.ds = red3.getInt("ds");
+            field.red3.radio = red3.getInt("radio");
+            field.red3.rio = red3.getInt("rio");
+            field.red3.code = red3.getInt("code");
+            field.red3.bwu = red3.getDouble("bwu");
+            field.red3.battery = red3.getDouble("battery");
+            field.red3.ping = red3.getInt("ping");
+            field.red3.packets = red3.getInt("packets");
+
+            if (root.isShown()) requireActivity().runOnUiThread(() -> monitorViewModel.getField().setValue(field));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }

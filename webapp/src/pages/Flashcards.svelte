4b873bead2d7157754a1dfb42db9d534db6dcdda
Filip Kin <me@filipkin.com>
2024-02-12 18:54:47 -0500

    Flashcards

---
+++ webapp/src/pages/Flashcards.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Button } from "flowbite-svelte";
+  import { flashcardsStore } from "../stores/flashcards";
+  import { get } from "svelte/store";
+
+    let flashcards = get(flashcardsStore);
+</script>
+
+<div class="overlay flex flex-col justify-center items-center">
+    <h1 class="text-3xl font-bold">Flashcards</h1>
+    <p class="text-lg">Select a flashcard to view</p>
+</div>
+
+<div class="space-y-2 p-4">
+    {#each flashcards as card}
+        <div>
+            <Button pill class="dark:bg-primary w-full">{card}</Button>
+        </div>
+    {/each}
+</div>

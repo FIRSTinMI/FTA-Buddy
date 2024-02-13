25e4a4cfb4af42b817abcc99e26446b7b29cf0e7
Filip Kin <me@filipkin.com>
2024-02-13 14:04:29 -0500

    Better navigation

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-    import { Router, Route } from "svelte-routing";
+    import { Router, Route, Link } from "svelte-routing";
     import Home from "./pages/Home.svelte";
     import Flashcard from "./pages/Flashcards.svelte";
     import { Button } from "flowbite-svelte";
     import Icon from "@iconify/svelte";
     import Reference from "./pages/Reference.svelte";
-
-    let page = window.location.pathname;
 </script>
 
 <main class="min-h-screen h-full bg-neutral-800 w-screen">
@@ -14,20 +12,31 @@
         <Route path="/" component={Home} />
         <Route path="/flashcards" component={Flashcard} />
         <Route path="/references" component={Reference} />
+
+        <div
+            class="flex justify-around py-2 fixed w-full bottom-0 bg-neutral-700"
+            id="nav-bar"
+        >
+            <Link to="/app/">
+                <Button class="!p-2">
+                    <Icon icon="mdi:television" class="w-8 h-8" />
+                </Button>
+            </Link>
+            <Link to="/app/flashcards">
+                <Button class="!p-2">
+                    <Icon icon="mdi:message-alert" class="w-8 h-8" />
+                </Button>
+            </Link>
+            <Link to="/app/references">
+                <Button class="!p-2">
+                    <Icon icon="mdi:file-document-outline" class="w-8 h-8" />
+                </Button>
+            </Link>
+            <Link to="/app/notes">
+                <Button class="!p-2">
+                    <Icon icon="mdi:message-text" class="w-8 h-8" />
+                </Button>
+            </Link>
+        </div>
     </Router>
 </main>
-
-<div class="flex justify-around py-2 fixed w-full bottom-0 bg-neutral-700" id="nav-bar">
-    <Button class="!p-2 {(page == '/') ? 'active' : ''}" href="./">
-        <Icon icon="mdi:television" class="w-8 h-8" />
-    </Button>
-    <Button class="!p-2 {(page == '/flashcards') ? 'active' : ''}" href="./flashcards">
-        <Icon icon="mdi:message-alert" class="w-8 h-8" />
-    </Button>
-    <Button class="!p-2 {(page == '/references') ? 'active' : ''}" href="./references">
-        <Icon icon="mdi:file-document-outline" class="w-8 h-8" />
-    </Button>
-    <Button class="!p-2 {(page == '/notes') ? 'active' : ''}" href="./notes" disabled>
-        <Icon icon="mdi:message-text" class="w-8 h-8" />
-    </Button>
-</div>

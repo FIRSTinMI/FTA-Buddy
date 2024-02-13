2204fc89609ed8393c8ccae472231022f01b782a
Filip Kin <me@filipkin.com>
2024-02-12 19:15:20 -0500

    Flashcards work

--- webapp/src/pages/Flashcards.svelte
+++ webapp/src/pages/Flashcards.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
-  import { Button } from "flowbite-svelte";
-  import { flashcardsStore } from "../stores/flashcards";
-  import { get } from "svelte/store";
+    import { Button } from "flowbite-svelte";
+    import { flashcardsStore } from "../stores/flashcards";
+    import { get } from "svelte/store";
 
     let flashcards = get(flashcardsStore);
+    let currentFlashcard = "";
+
+    function openFlashcard(e: MouseEvent) {
+        const target = e.target as HTMLButtonElement;
+        const flashcard = target.textContent;
+        currentFlashcard = flashcard || "";
+    }
+
+    function dismissFlashcard() {
+        currentFlashcard = "";
+    }
 </script>
 
-<div class="overlay flex flex-col justify-center items-center">
-    <h1 class="text-3xl font-bold">Flashcards</h1>
-    <p class="text-lg">Select a flashcard to view</p>
-</div>
+{#key currentFlashcard}
+    {#if currentFlashcard}
+        <div class="overlay flex flex-col justify-center items-center" on:click={dismissFlashcard} on:keydown={dismissFlashcard} role="presentation" id="flashcard-overlay">
+            <h1 class="text-3xl font-bold">{currentFlashcard}</h1>
+        </div>
+    {/if}
+{/key}
 
 <div class="space-y-2 p-4">
     {#each flashcards as card}
         <div>
-            <Button pill class="dark:bg-primary w-full">{card}</Button>
+            <Button pill class="dark:bg-primary w-full" on:click={openFlashcard}>{card}</Button>
         </div>
     {/each}
 </div>

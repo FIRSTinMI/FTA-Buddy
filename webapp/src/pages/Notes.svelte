fabfe47cf18103f5eb1055ac42dd6625aa921c0e
Filip Kin <me@filipkin.com>
2024-02-13 20:07:44 -0500

    notes

--- webapp/src/pages/Notes.svelte
+++ webapp/src/pages/Notes.svelte
@@ -1,39 +1,56 @@
 <script lang="ts">
+    import { notesStore } from "./../stores/notes";
     import { Label, Select, type SelectOptionType } from "flowbite-svelte";
     import { eventStore } from "../stores/event";
     import { get } from "svelte/store";
-    import type { SelectEvents } from "flowbite-svelte/Select.svelte";
-    export let team: string;
-    let event = get(eventStore);
-    
-    console.log(team, event);
+    import Messaging from "../components/Messaging.svelte";
 
-    async function updateTeams() {
-        const teamsData = await fetch("https://ftahelper.filipkin.com/teams/"+encodeURIComponent(event))
-            .then((res) => res.json());
-        console.log(teamsData);
-        teams = teamsData.map((team: string) => ({ name: team.toString(), value: team.toString() }));
-        console.log(teams);
-        return teams;
+    let event = get(eventStore);
+    let notesStoreData = get(notesStore);
+
+    export let team: string;
+    if (team === undefined) {
+        team = notesStoreData.team;
     }
 
-    let teams: SelectOptionType<string>[] = [];
+    let teams: SelectOptionType<string>[] = notesStoreData.teams;
+    updateTeams();
+
+    async function updateTeams() {
+        const teamsData = await fetch(
+            "https://ftabuddy.com/teams/" + encodeURIComponent(event),
+        ).then((res) => res.json());
+        teams = teamsData.map((team: string) => ({
+            name: team.toString(),
+            value: team.toString(),
+        }));
+        notesStoreData.teams = teams;
+        notesStore.set(notesStoreData);
+        console.log(teams);
+    }
 
     function selectTeam(evt: Event) {
-        console.log(evt);
-        
+        team = (evt.target as HTMLSelectElement).selectedOptions[0].value;
+        notesStoreData.team = team;
+        notesStore.set(notesStoreData);
     }
 </script>
 
-<div class="container mx-auto p-2">
+<div class="container mx-auto p-2 h-full flex flex-col">
     <form class="flex w-full">
         <Label class="w-full text-left">
             Select Team
-            {#await updateTeams()}
-                Loading teams...
-            {:then teams} 
-                <Select class="mt-2" items={teams} bind:value={team} on:change={selectTeam}/>
-            {/await}
+            <Select
+                class="mt-2"
+                items={teams}
+                bind:value={team}
+                on:change={selectTeam}
+            />
         </Label>
     </form>
+    {#key team}
+        {#if team}
+            <Messaging {team} />
+        {/if}
+    {/key}
 </div>

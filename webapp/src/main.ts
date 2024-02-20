a096559c63d0fb01e08311474b1b5585b074cf6b
Filip Kin <me@filipkin.com>
2024-02-20 12:31:10 -0500

    Add service worker

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,7 +2,11 @@ import "./app.pcss";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+    target: document.getElementById("app"),
 });
 
 export default app;
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/app/serviceworker.js");
+}

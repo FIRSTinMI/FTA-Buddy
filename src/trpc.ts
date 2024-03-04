25e215fe2089baf0d3a751d1a730656a33adb3f7
Filip Kin <filipk@acumenapps.co.nz>
2024-03-03 23:50:24 -0500

    trpc

--- src/trpc.ts
+++ src/trpc.ts
@@ -1,6 +1,9 @@
 import { initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
-const t = initTRPC.create();
+export const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = Awaited<ReturnType<typeof createContext>>;
+const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;

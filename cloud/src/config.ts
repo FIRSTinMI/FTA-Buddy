86f71f689d89de6067ba099d776aabd06b7617ea
Filip Kin <filipk@acumenapps.co.nz>
2024-03-03 23:05:12 -0500

    Major refactor

--- cloud/src/config.ts
+++
@@ -1,7 +0,0 @@
-export const DATABASE_CONFIG = {
-    host: "localhost",
-    user: "foo",
-    password: "bar",
-    database: "db"
-};
-export const TBA_KEY = "foo";
---
+++ src/db/db.ts
@@ -0,0 +1,21 @@
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+import * as schema from "./schema";
+import 'dotenv/config';
+
+export let db: NodePgDatabase<typeof schema>;
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT ?? '5432')
+});
+if (!client) throw new Error("Failed to create database connection");
+
+export function connect() {
+    return client.connect().then(() => {
+        db = drizzle(client, { schema });
+        console.log("Connected to database")
+    }).catch(console.error);
+}

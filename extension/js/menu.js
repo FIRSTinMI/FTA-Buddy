455f145fb665b30bf17431db7fbe85ab7e62f477
Filip Kin <filipk@acumenapps.co.nz>
2024-03-20 00:59:52 -0400

    Disable extension after 4 days

--- extension/js/menu.js
+++ extension/js/menu.js
@@ -3,43 +3,44 @@ const urlInput = document.getElementById('url');
 const urlContainer = document.getElementById('url-container');
 const eventInput = document.getElementById('event');
 const eventContainer = document.getElementById('event-container');
+const enabled = document.getElementById('enabled');
 
-chrome.storage.local.get(['url', 'cloud', 'event'], item => {
+chrome.storage.local.get(['url', 'cloud', 'event', 'changed', 'enabled'], item => {
     console.log(item);
 
-    if (item.url == undefined || item.cloud == undefined || item.event == undefined || item.changed == undefined) {
-        item = { url: item.url || '127.0.0.1', cloud: item.cloud || true, event: item.event || '2024event', changed: item.changed || new Date().getTime() };
+    if (item.url == undefined || item.cloud == undefined || item.event == undefined || item.changed == undefined || item.enabled == undefined) {
+        item = {
+            url: item.url || 'ws://localhost:3001/ws/',
+            cloud: item.cloud ?? true,
+            event: item.event || '2024event',
+            changed: item.changed || new Date().getTime(),
+            enabled: item.enabled ?? false
+        };
         chrome.storage.local.set(item);
     }
     cloudCheckbox.checked = item.cloud;
     urlInput.value = item.url;
     eventInput.value = item.event;
+    enabled.checked = item.enabled;
 
-    if (item.cloud) {
-        urlContainer.style.display = 'none';
-        eventContainer.style.display = 'block';
-    } else {
-        urlContainer.style.display = 'block';
-        eventContainer.style.display = 'none';
-    }
+    urlContainer.style.display = item.cloud ? 'none' : 'block';
 
     if (item.changed + (1000 * 60 * 60 * 24 * 4) < new Date().getTime()) {
-        item.event = '2024event';
-        item.changed = new Date();
-        chrome.storage.local.set(item);
+        enabled.checked = false;
+        chrome.storage.local.set({ enabled: false });
     }
 });
 
 function handleUpdate() {
-    if (eventInput.value == '' || eventInput.value == 'test') {
+    if (eventInput.value == '') {
         eventInput.value = '2024event';
     }
     if (urlInput.value == '') {
-        urlInput.value = '127.0.0.1';
+        urlInput.value = 'ws://localhost:3001/ws/';
     }
 
-    console.log({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime() })
-    chrome.storage.local.set({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime() });
+    console.log({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime(), enabled: enabled.checked })
+    chrome.storage.local.set({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime(), enabled: enabled.checked });
 
     if (cloudCheckbox.checked) {
         urlContainer.style.display = 'none';
@@ -53,3 +54,4 @@ function handleUpdate() {
 cloudCheckbox.addEventListener('input', handleUpdate);
 urlInput.addEventListener('input', handleUpdate);
 eventInput.addEventListener('input', handleUpdate);
+enabled.addEventListener('input', handleUpdate);

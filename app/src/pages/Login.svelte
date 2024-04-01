6f2e253b59fd8ad21e2659f31099ac26e7a5d52f
Filip Kin <me@filipkin.com>
2024-03-31 21:04:59 -0400

    delay switch

--- app/src/pages/Login.svelte
+++ app/src/pages/Login.svelte
@@ -186,7 +186,7 @@
                 teams: (res.teams as any) || [],
             });
             toast("Success", "Event joined successfully", "green-500");
-            setTimeout(() => navigate("/app/"), 50);
+            setTimeout(() => navigate("/app/"), 200);
         } catch (err: any) {
             toast("Error Joining Event", err.message);
             console.error(err);

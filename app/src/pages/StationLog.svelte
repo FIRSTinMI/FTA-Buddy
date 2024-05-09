801ea8e9d6d7e76e10cb7c44c6bf03ac78b85410
Filip Kin <filipk@acumenapps.co.nz>
2024-05-08 23:55:28 -0400

    mobile optimized match logs

--- app/src/pages/StationLog.svelte
+++ app/src/pages/StationLog.svelte
@@ -17,6 +17,7 @@
     export let station: ROBOT;
 
     let log: FMSLogFrame[];
+    let team: number;
 
     const match = trpc.match.getMatch.query({ id: matchid });
 
@@ -24,6 +25,7 @@
         // I don't want to talk about it
         log =
             (m?.[(station + "_log") as keyof typeof m] as FMSLogFrame[]) ?? [];
+        team = m?.[station] ?? 0;
     });
 
     function back() {
@@ -35,59 +37,80 @@
     }
 </script>
 
-<div class="container mx-auto p-2 w-full flex flex-col gap-4">
-    <Button on:click={back} class="w-fit">Back</Button>
+<div class="container mx-auto p-0 py-2 md:p-2 w-full flex flex-col gap-2 md:gap-4">
+    <Button on:click={back} class="w-fit mx-1.5">Back</Button>
     {#await match}
         <Spinner />
     {:then match}
-        <Table>
+        <h1 class="text-xl">{match?.level === 'None' ? 'Test' : match?.level} Match {match?.match_number}/{match?.play_number}</h1>
+        <h2 class="text-lg">{(station.startsWith('blue') ? 'Blue ' : 'Red ') + station.charAt(station.length - 1)} Team {team}</h2>
+        <Table class="log-table">
             <TableHead>
-                <TableHeadCell>Time</TableHeadCell>
-                <TableHeadCell>DS</TableHeadCell>
-                <TableHeadCell>Radio</TableHeadCell>
-                <TableHeadCell>RIO</TableHeadCell>
-                <TableHeadCell>Code</TableHeadCell>
-                <TableHeadCell>Enabled</TableHeadCell>
-                <TableHeadCell>A Stop</TableHeadCell>
-                <TableHeadCell>E Stop</TableHeadCell>
-                <TableHeadCell>Battery</TableHeadCell>
-                <TableHeadCell>Ping</TableHeadCell>
-                <TableHeadCell>BWU</TableHeadCell>
-                <TableHeadCell>Dropped Packets</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Time</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">DS</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Radio</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">RIO</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Code</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Status</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Bat</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Ping</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">BWU</TableHeadCell>
+                <TableHeadCell class="px-6 py-4">Lost Pkts</TableHeadCell>
             </TableHead>
             <TableBody>
                 {#if match}
                     {#each log as frame}
                         <TableBodyRow>
-                            <TableBodyCell>{frame.matchTimeBase}</TableBodyCell>
+                            <TableBodyCell>{frame.matchTime}</TableBodyCell>
                             <TableBodyCell
                                 class={frame.dsLinkActive ? "" : "bg-red-500"}
-                                >{frame.dsLinkActive}</TableBodyCell
+                                >{frame.dsLinkActive ? "Y" : "N"}</TableBodyCell
                             >
                             <TableBodyCell
                                 class={frame.radioLink ? "" : "bg-red-500"}
-                                >{frame.radioLink}</TableBodyCell
+                                >{frame.radioLink ? "Y" : "N"}</TableBodyCell
                             >
                             <TableBodyCell
                                 class={frame.rioLink ? "" : "bg-red-500"}
-                                >{frame.rioLink}</TableBodyCell
+                                >{frame.rioLink ? "Y" : "N"}</TableBodyCell
                             >
                             <TableBodyCell
                                 class={frame.linkActive ? "" : "bg-red-500"}
-                                >{frame.linkActive}</TableBodyCell
+                                >{frame.linkActive ? "Y" : "N"}</TableBodyCell
                             >
-                            <TableBodyCell
-                                class={frame.enabled ? "" : "bg-red-500"}
-                                >{frame.enabled}</TableBodyCell
-                            >
-                            <TableBodyCell
-                                class={frame.aStopPressed ? "" : "bg-red-500"}
-                                >{frame.aStopPressed}</TableBodyCell
-                            >
-                            <TableBodyCell
-                                class={frame.eStopPressed ? "" : "bg-red-500"}
-                                >{frame.eStopPressed}</TableBodyCell
-                            >
+                            {#if frame.eStopPressed}
+                                <TableBodyCell
+                                    class="bg-red-500"
+                                    >E</TableBodyCell
+                                >
+                            {:else if frame.aStopPressed}
+                                <TableBodyCell
+                                    class="bg-orange-500"
+                                    >A</TableBodyCell
+                                >
+                            {:else}
+                                {#if !frame.enabled}
+                                    <TableBodyCell
+                                        class="bg-red-500"
+                                        >N</TableBodyCell
+                                    >
+                                {:else}
+                                    {#if frame.auto}
+                                        <TableBodyCell
+                                            >A</TableBodyCell
+                                        >
+                                    {:else if frame.teleop}
+                                        <TableBodyCell
+                                            >T</TableBodyCell
+                                        >
+                                    {:else}
+                                        <TableBodyCell
+                                            class="bg-blue-500"
+                                            >E</TableBodyCell
+                                        >
+                                    {/if}
+                                {/if}
+                            {/if}
                             <TableBodyCell
                                 style="background-color: rgba(255,0,0,{frame.battery <
                                     11 && frame.battery > 0

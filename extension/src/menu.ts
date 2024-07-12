f195fc100212f2c13f178fd7a4e6d9af314d0e50
Filip Kin <me@filipkin.com>
2024-07-12 10:26:41 -0400

    Extension bug fixes

--- extension/src/menu.ts
+++ extension/src/menu.ts
@@ -5,27 +5,31 @@ const eventInput = document.getElementById('event') as HTMLInputElement;
 const eventContainer = document.getElementById('event-container') as HTMLDivElement;
 const enabled = document.getElementById('enabled') as HTMLInputElement;
 const signalREnabled = document.getElementById('signalR') as HTMLInputElement;
+const tokenInput = document.getElementById('eventToken') as HTMLInputElement;
 
 function load() {
-    chrome.storage.local.get(['url', 'cloud', 'event', 'changed', 'enabled', 'signalR'], item => {
+    chrome.storage.local.get(['url', 'cloud', 'event', 'eventToken', 'changed', 'enabled', 'signalR'], item => {
         console.log(item);
 
-        if (item.url == undefined || item.cloud == undefined || item.event == undefined || item.changed == undefined || item.enabled == undefined || item.signalR == undefined) {
+        if (item.url == undefined || item.cloud == undefined || item.event == undefined || item.changed == undefined || item.enabled == undefined || item.signalR == undefined || item.eventToken == undefined) {
             item = {
                 url: item.url || 'ws://localhost:3001/ws/',
                 cloud: item.cloud ?? true,
                 event: item.event || '2024event',
                 changed: item.changed || new Date().getTime(),
                 enabled: item.enabled ?? false,
-                signalR: item.signalR ?? false
+                signalR: item.signalR ?? false,
+                eventToken: item.eventToken || ''
             };
             chrome.storage.local.set(item);
         }
+
         cloudCheckbox.checked = item.cloud;
         urlInput.value = item.url;
         eventInput.value = item.event;
         enabled.checked = item.enabled;
         signalREnabled.checked = item.signalR;
+        tokenInput.value = item.eventToken;
 
         urlContainer.style.display = item.cloud ? 'none' : 'block';
 
@@ -39,6 +43,7 @@ function load() {
         eventInput.addEventListener('input', handleUpdate);
         enabled.addEventListener('input', handleUpdate);
         signalREnabled.addEventListener('input', handleUpdate);
+        tokenInput.addEventListener('input', handleUpdate);
     });
 }
 
@@ -50,13 +55,13 @@ function handleUpdate() {
         urlInput.value = 'ws://localhost:3001/ws/';
     }
 
-    console.log({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime(), enabled: enabled.checked, signalR: signalREnabled.checked})
-    chrome.storage.local.set({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime(), enabled: enabled.checked, signalR: signalREnabled.checked});
+    console.log({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime(), enabled: enabled.checked, signalR: signalREnabled.checked, eventToken: tokenInput.value });
+    chrome.storage.local.set({ url: urlInput.value, cloud: cloudCheckbox.checked, event: eventInput.value, changed: new Date().getTime(), enabled: enabled.checked, signalR: signalREnabled.checked, eventToken: tokenInput.value });
 
     urlContainer.style.display = cloudCheckbox.checked ? 'none' : 'block';
 }
 
-function updatePopup(setting: 'url' | 'cloud' | 'enabled' | 'event' | 'signalR', value: boolean | string) {
+function updatePopup(setting: 'url' | 'cloud' | 'enabled' | 'event' | 'signalR' | 'eventToken', value: boolean | string) {
     const elm = document?.getElementById(setting);
     if (!elm) return;
     if (typeof value === 'boolean') {
@@ -69,7 +74,7 @@ function updatePopup(setting: 'url' | 'cloud' | 'enabled' | 'event' | 'signalR',
 chrome.storage.local.onChanged.addListener((changes) => {
     for (const key of Object.keys(changes)) {
         if (key === 'changed') continue;
-        updatePopup(key as 'url' | 'cloud' | 'enabled' | 'event' | 'signalR', changes[key].newValue);
+        updatePopup(key as 'url' | 'cloud' | 'enabled' | 'event' | 'signalR' | 'eventToken', changes[key].newValue);
     }
 });
 

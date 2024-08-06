9b8b583233eb1ef211fb4fee0712c0c4541622a8
Filip Kin <filipk@acumenapps.co.nz>
2024-08-06 12:38:33 -0400

    Responsive field monitor

--- app/src/pages/Home.svelte
+++ app/src/pages/Home.svelte
@@ -116,44 +116,28 @@
     <TeamModal bind:modalOpen {modalStation} {monitorFrame} {frameHandler} />
 {/if}
 
-<div class="w-full mx-auto {fullscreen ? "" : "container md:max-w-2xl"} md:p-2 space-y-2">
+<div class="grid grid-cols-fieldmonitor lg:grid-cols-fieldmonitor-large gap-0.5 md:gap-1 lg:gap-2 mx-auto justify-center {fullscreen && "fullscreen"}">
     {#key monitorFrame}
-        <div class="flex w-full mb-1">
-            {#if monitorFrame}
-                <div class="w-42 {fullscreen ? "text-6xl" : "md:text-2xl"}">M: {monitorFrame.match}</div>
-                <div class="grow {fullscreen ? "text-6xl" : "md:text-2xl"}">{FieldStates[monitorFrame.field]}</div>
-                <div class="w-42 {fullscreen ? "text-6xl" : "md:text-2xl"}">{monitorFrame.time}</div>
-            {/if}
-        </div>
-        <Table class="w-full mx-auto !overflow-none monitor max-h-screen">
-            <TableHead class="dark:bg-neutral-500 dark:text-white text-center">
-                {#if !fullscreen}
-                <TableHeadCell class="w-20">Team</TableHeadCell>
-                <TableHeadCell class="w-20">DS</TableHeadCell>
-                <TableHeadCell class="w-10 lg:w-20">Radio</TableHeadCell>
-                <TableHeadCell class="w-10 lg:w-20">Rio</TableHeadCell>
-                <TableHeadCell class="w-15 lg:w-20 max-w-20 lg:max-w-24">Bat</TableHeadCell>
-                <TableHeadCell>Net</TableHeadCell>
-                {:else}
-                <TableHeadCell class="w-38">Team</TableHeadCell>
-                <TableHeadCell class="w-38">DS</TableHeadCell>
-                <TableHeadCell class="w-38">Radio</TableHeadCell>
-                <TableHeadCell class="w-38">Rio</TableHeadCell>
-                <TableHeadCell class="w-38">Bat</TableHeadCell>
-                <TableHeadCell class="w-fit">Ping</TableHeadCell>
-                <TableHeadCell class="w-fit">BWU</TableHeadCell>
-                <TableHeadCell class="w-38">Signal</TableHeadCell>
-                {/if}
-            </TableHead>
-            <TableBody>
-                {#if monitorFrame}
-                    {#each stations as station}
-                        <MonitorRow {station} {monitorFrame} {detailView} {fullscreen} {frameHandler} />
-                    {/each}
-                {/if}
-            </TableBody>
-        </Table>
-        <div class="flex w-full mt-1">
+        {#if monitorFrame}
+            <div class="col-span-6 lg:col-span-8 flex text-lg md:text-2xl font-semibold {fullscreen && "lg:text-6xl"}">
+                <div class="px-2">M: {monitorFrame.match}</div>
+                <div class="flex-1 px-2 text-center">{FieldStates[monitorFrame.field]}</div>
+                <div class="px-2">{monitorFrame.time}</div>
+            </div>
+            <p>Team</p>
+            <p>DS</p>
+            <p>Radio</p>
+            <p>Rio</p>
+            <p>Battery</p>
+            <p class="hidden lg:flex">Ping (ms)</p>
+            <p class="hidden lg:flex">BWU (mbps)</p>
+            <p class="hidden lg:flex">Signal (dBm)</p>
+            <p class="lg:hidden">Net</p>
+            {#each stations as station}
+                <MonitorRow {station} {monitorFrame} {detailView} {fullscreen} {frameHandler} />
+            {/each}
+        {/if}
+        <div class="col-span-6 lg:col-span-8 flex text-lg md:text-2xl font-semibold {fullscreen && "lg:text-4xl"}">
             <div class="w-48 text-left {fullscreen ? "text-4xl" : "md:text-2xl"} {currentCycleIsBest && "text-green-500"}">C: {lastCycleTime}</div>
             <div class="grow {fullscreen ? "text-4xl" : "md:text-2xl"}"></div>
             <div 

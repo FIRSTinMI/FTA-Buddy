06f4d1d3cf8f832a3fa59654e37f6f698c8f29d9
kelmalon <110502316+kelmalon@users.noreply.github.com>
2024-12-24 21:52:20 -0500

    left behind from previous commit

--- app/src/pages/StatusLights.svelte
+++ app/src/pages/StatusLights.svelte
@@ -73,7 +73,7 @@
 </script>
 
 <div class="container mx-auto p-2 pr-3 w-full">
-    <h1 class="text-3xl">Status Lights</h1>
+    <h1 class="text-3xl" style="font-weight: bold">Status Lights</h1>
     <Accordion flush class="text-left">
         <AccordionItem class="text-black dark:text-white">
             <span slot="header">Robot Radio</span>

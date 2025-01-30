ba43225d695b220f6f3958a0e9436a1610549a00
Filip Kin <filipk@acumenapps.co.nz>
2025-01-30 16:52:13 -0500

    Hide fullscreen button on small devices

--- app/src/pages/Monitor.svelte
+++ app/src/pages/Monitor.svelte
@@ -197,7 +197,7 @@
 				<div class="px-2">{monitorFrame.time}</div>
 				<Button
 					color="none"
-					class="text-sm {!fullscreen && 'fixed top-12 right-0 z-50'}"
+					class="hidden text-sm {!fullscreen && 'md:block fixed top-12 right-0 z-50'}"
 					on:click={(evt) => {
 						evt.preventDefault();
 						fullscreen = !fullscreen;

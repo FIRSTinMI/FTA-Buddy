9b773bc7d84bc1fa373c34a0e1854427fe15d26e
Filip Kin <me@filipkin.com>
2025-02-27 15:29:38 -0500

    Update field manual

--- app/src/pages/references/FieldManuals.svelte
+++ app/src/pages/references/FieldManuals.svelte
@@ -10,56 +10,70 @@
 	<Accordion flush class="text-left">
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Pack Reef Cart</span>
-			<a href="/app/pdf/reefcase.pdf" target="_blank"><QrCode value="https://ftabuddy.com/app/pdf/reefcase.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/REDREEF" target="_blank"><QrCode value="https://roadcase.frc.tools/REDREEF" padding={12} /></a>
 		</AccordionItem>
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Pack Case 23</span>
-			<a href="/app/pdf/23.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/23.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/23" target="_blank"><QrCode value="https://roadcase.frc.tools/23" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Pack Case 24</span>
-			<a href="/app/pdf/24.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/24.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/24" target="_blank"><QrCode value="https://roadcase.frc.tools/24" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
-			<span slot="header"> Pack Case 32 Practice Field </span>
-			<a href="/app/pdf/32.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/32.pdf" padding={12} /></a>
+			<span slot="header"> Pack Case 31 Practice Field </span>
+			<a href="https://roadcase.frc.tools/31" target="_blank"><QrCode value="https://roadcase.frc.tools/31" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Carpet and Border</span>
-			<a href="/app/pdf/carpet.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/carpet.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/BorderSetup" target="_blank"><QrCode value="https://roadcase.frc.tools/BorderSetup" padding={12} /></a>
+		</AccordionItem>
+
+		<AccordionItem class="text-black dark:text-white">
+			<span slot="header">Field Marking and Tape</span>
+			<a href="https://roadcase.frc.tools/FieldMarkings" target="_blank"><QrCode value="https://roadcase.frc.tools/FieldMarkings" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Build Coral Station</span>
-			<a href="/app/pdf/coral.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/coral.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/CoralStationSetup" target="_blank"
+				><QrCode value="https://roadcase.frc.tools/CoralStationSetup" padding={12} /></a
+			>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Build Processor</span>
-			<a href="/app/pdf/processor.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/processor.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/ProcessorSetup" target="_blank"><QrCode value="https://roadcase.frc.tools/ProcessorSetup" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Build Barge</span>
-			<a href="/app/pdf/barge.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/barge.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/BargeSetup" target="_blank"><QrCode value="https://roadcase.frc.tools/BargeSetup" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Build Reef</span>
-			<a href="/app/pdf/reef.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/reef.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/ReefSetup" target="_blank"><QrCode value="https://roadcase.frc.tools/ReefSetup" padding={12} /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Build Practice Field</span>
-			<a href="/app/pdf/practice.pdf" target="_blank"> <QrCode value="https://ftabuddy.com/app/pdf/practice.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/PracticeFieldSetup" target="_blank"
+				><QrCode value="https://roadcase.frc.tools/PracticeFieldSetup" padding={12} /></a
+			>
+		</AccordionItem>
+
+		<AccordionItem class="text-black dark:text-white">
+			<span slot="header">Scoring Table</span>
+			<a href="https://roadcase.frc.tools/ScoringTableSetup"><QrCode value="https://roadcase.frc.tools/ScoringTableSetup" /></a>
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
 			<span slot="header">Full Manual</span>
-			<a href="/app/pdf/2025.pdf" target="_blank"> <QrCode value="/app/pdf/2025.pdf" padding={12} /></a>
+			<a href="https://roadcase.frc.tools/FieldManual" target="_blank"><QrCode value="https://roadcase.frc.tools/FieldManual" padding={12} /></a>
 		</AccordionItem>
 	</Accordion>
 </div>

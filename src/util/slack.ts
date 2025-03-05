cabc0e5d13eb7a655919a0f197565c439c72efbd
Filip Kin <filipk@acumenapps.co.nz>
2025-03-05 15:03:54 -0500

    Create ticket message

--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,6 +1,7 @@
 import { eq } from "drizzle-orm";
 import { db } from "../db/db";
 import { events, slackServers } from "../db/schema";
+import { getEvent } from "./get-event";
 
 export async function slackOAuth(code: string) {
     const url = new URL('https://slack.com/api/oauth.v2.access');
@@ -30,7 +31,6 @@ export async function slackOAuth(code: string) {
 
     return true;
 }
-
 export async function linkChannel(args: string[], channel_id: string, team_id: string) {
     if (!args[1]) {
         throw new Error("Usage: `/ftabuddy link [event code]`");
@@ -38,13 +38,105 @@ export async function linkChannel(args: string[], channel_id: string, team_id: s
 
     const eventCode = args[1];
 
-    const event = (await db.select({ code: events.code, pin: events.pin }).from(events).where(eq(events.code, eventCode)).execute())[0];
+    // Check if the event exists
+    const event = (await db.select({ code: events.code, pin: events.pin })
+        .from(events)
+        .where(eq(events.code, eventCode))
+        .execute())[0];
 
     if (!event) {
         throw new Error("Event not found");
     }
 
-    await db.update(events).set({ slackChannel: channel_id, slackTeam: team_id }).where(eq(events.code, eventCode)).execute();
+    // Check if the bot is in the channel
+    const botInChannel = await isBotInChannel(channel_id, team_id);
 
-    return { text: `Linked event ${eventCode} to this channel, use pin ${event.pin} to join the event in the app!` };
+    if (!botInChannel) {
+        throw new Error(`Bot is not in channel, invite the bot using \`/invite @FTA Buddy\`, then try again!`);
+    }
+
+    // Store the channel in the database
+    await db.update(events)
+        .set({ slackChannel: channel_id, slackTeam: team_id })
+        .where(eq(events.code, eventCode))
+        .execute();
+
+    const eventObj = await getEvent("", eventCode);
+    eventObj.slackChannel = channel_id;
+    eventObj.slackTeam = team_id;
+
+    return {
+        response_type: "in_channel",
+        text: `Linked event ${eventCode} to this channel. Use pin ${event.pin} to join the event in the app!`
+    };
+
+}
+
+async function isBotInChannel(channel_id: string, team_id: string): Promise<boolean> {
+    console.log(await getTokenByTeam(team_id));
+    const response = await fetch(`https://slack.com/api/conversations.info?channel=${channel_id}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${await getTokenByTeam(team_id)}`,
+            "Content-Type": "application/json"
+        },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (!data.ok) {
+        return false;
+    }
+
+    return data.channel.is_member;
+}
+
+async function getTokenByTeam(team_id: string) {
+    return await db.select({ access_token: slackServers.access_token }).from(slackServers).where(eq(slackServers.team_id, team_id)).execute();
+}
+
+async function getChannelByEvent(eventCode: string) {
+    const result = (await db.select({ slackChannel: events.slackChannel, slackTeam: events.slackTeam }).from(events).where(eq(events.code, eventCode)).execute())[0];
+
+    if (!result) {
+        throw new Error("Event not found");
+    }
+
+    if (!result.slackChannel || !result.slackTeam) {
+        throw new Error("Event not linked to a channel");
+    }
+
+    return result as { slackChannel: string, slackTeam: string; };
+}
+
+export async function sendSlackMessage(channel_id: string, team_id: string, message: SlackMessage) {
+    const response = await fetch("https://slack.com/api/chat.postMessage", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${await getTokenByTeam(team_id)}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ channel: channel_id, ...message })
+    });
+
+    const data = await response.json();
+
+    if (!data.ok) {
+        throw new Error(data.error);
+    }
+
+    return data.ts;
+}
+
+export async function sendMessageToEventChannel(eventCode: string, message: SlackMessage) {
+    const { slackChannel, slackTeam } = await getChannelByEvent(eventCode);
+
+    await sendSlackMessage(slackChannel, slackTeam, message);
+}
+
+export interface SlackMessage {
+    blocks?: any[];
+    text?: string;
 }

a671a2ef002cc283a799682e06ef061974ce44b1
Filip Kin <filipk@acumenapps.co.nz>
2025-03-05 14:27:44 -0500

    Slack command fix

---
+++ drizzle/0043_stale_magma.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "events" ADD COLUMN "slackChannel" varchar;--> statement-breakpoint
+ALTER TABLE "events" ADD COLUMN "slackTeam" varchar;
--- src/util/slack.ts
+++ src/util/slack.ts
@@ -31,20 +31,9 @@ export async function slackOAuth(code: string) {
     return true;
 }
 
-export async function slackCommand(text: string, channel_id: string, response_url: string) {
-    const args = text.split(" ");
-
-    switch (args[0]) {
-        case "link":
-            return linkChannel(args, channel_id, response_url);
-        default:
-            return { text: "Invalid command" };
-    }
-}
-
-async function linkChannel(args: string[], channel_id: string, response_url: string) {
+export async function linkChannel(args: string[], channel_id: string, team_id: string) {
     if (!args[1]) {
-        return { text: "Usage: `/ftabuddy link [event code]`" };
+        throw new Error("Usage: `/ftabuddy link [event code]`");
     }
 
     const eventCode = args[1];
@@ -52,10 +41,10 @@ async function linkChannel(args: string[], channel_id: string, response_url: str
     const event = (await db.select({ code: events.code, pin: events.pin }).from(events).where(eq(events.code, eventCode)).execute())[0];
 
     if (!event) {
-        return { text: "Event not found" };
+        throw new Error("Event not found");
     }
 
-    await db.update(events).set({ slackChannel: channel_id }).where(eq(events.code, eventCode)).execute();
+    await db.update(events).set({ slackChannel: channel_id, slackTeam: team_id }).where(eq(events.code, eventCode)).execute();
 
     return { text: `Linked event ${eventCode} to this channel, use pin ${event.pin} to join the event in the app!` };
 }

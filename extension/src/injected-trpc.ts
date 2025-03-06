a6b3ac71c842ebeb6010b52bbbf9ebe4e3d96269
Filip Kin <me@filipkin.com>
2025-03-06 17:55:12 -0500

    Nexus inspection status scraper

---
+++ extension/src/injected-trpc.ts
@@ -0,0 +1,58 @@
+import { createTRPCClient, createWSClient, httpBatchLink, splitLink, wsLink } from '@trpc/client';
+import type { AppRouter } from '../../src/index';
+import SuperJSON from 'superjson';
+
+let cloud: boolean = true;
+let id: string = '';
+let eventCode: string = '';
+let url: string = '';
+let eventToken: string = '';
+
+let linkURL = (cloud ? 'wss://ftabuddy.com/ws' : (url.replace('http', 'ws') + '/ws'));
+console.log(linkURL);
+
+let wsClient = createWSClient({
+    url: linkURL,
+});
+
+export async function updateValues(item: { cloud: boolean, id: string, event: string, url: string, eventToken: string; }) {
+    cloud = item.cloud;
+    id = item.id;
+    eventCode = item.event;
+    url = item.url;
+    eventToken = item.eventToken;
+
+    linkURL = (cloud ? 'wss://ftabuddy.com/ws' : (url.replace('http', 'ws') + '/ws'));
+    console.log(linkURL);
+
+    wsClient = createWSClient({
+        url: linkURL,
+    });
+
+    trpc = createTRPCConnection();
+}
+
+function createTRPCConnection() {
+    return createTRPCClient<AppRouter>({
+        links: [
+            splitLink({
+                condition(op) {
+                    return op.type === 'subscription' || op.path === 'field.post' || op.path === 'match.putMatchLogs';
+                },
+                true: wsLink({
+                    client: wsClient,
+                    transformer: SuperJSON
+                }),
+                false: httpBatchLink({
+                    url: (cloud ? 'https://ftabuddy.com/trpc' : url + '/trpc'),
+                    transformer: SuperJSON,
+                    headers: {
+                        'Event-Token': eventToken ?? '',
+                    }
+                }),
+            })
+        ],
+    });
+}
+
+export let trpc = createTRPCConnection();

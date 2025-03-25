82cc12f564306b9a71771298ab7307475731fced
Filip Kin <me@filipkin.com>
2025-03-25 11:44:54 -0400

    Update vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { visualizer } from 'rollup-plugin-visualizer';
 import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
@@ -9,13 +8,7 @@ export default defineConfig({
         svelte(),
         Icons({
             compiler: 'svelte',
-        }),
-        visualizer({
-            open: true, // opens the report in your browser automatically
-            gzipSize: true,
-            brotliSize: true,
-            filename: 'build-stats.html',
-        }),
+        })
     ],
     base: "/app/",
 });

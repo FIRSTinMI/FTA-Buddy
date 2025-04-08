92adfeee6ea128401a185efd5b364288aa7cec64
kelmalon <110502316+kelmalon@users.noreply.github.com>
2025-04-07 20:52:36 -0400

    Edited Status Lights, Component Manuals, and Wiirng Diagrams

--- app/src/pages/references/ComponentManuals.svelte
+++ app/src/pages/references/ComponentManuals.svelte
@@ -12,6 +12,8 @@
 		victorspx: false,
 		canivore: false,
 		pigeon: false,
+		pdh: false,
+		cancoder: false
 	};
 
 	let loadedState = {
@@ -28,6 +30,8 @@
 		if (openState.victorspx) loadedState.victorspx = true;
 		if (openState.canivore) loadedState.canivore = true;
 		if (openState.pigeon) loadedState.pigeon = true;
+		if (openState.pdh) loadedState.pdh = true;
+		if (openState.cancoder) loadedState.cancoder = true;
 	}
 
 	addEventListener("load", async () => {
@@ -42,6 +46,8 @@
 		loadedState.victorspx = true;
 		loadedState.canivore = true;
 		loadedState.pigeon = true;
+		loadedState.pdh = true;
+		loadedState.cancoder = true;
 	});
 </script>
 
@@ -51,20 +57,20 @@
 
 	<Accordion flush class="text-left">
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.radio}>
-			<span slot="header">FRC Robot Radio</span>
+			<span slot="header" class="font-bold">FRC Robot Radio</span>
 			{#if openState.radio === true || loadedState.radio === true}
 				<a href="https://frc-radio.vivid-hosting.net/" target="_blank"><QrCode value="https://frc-radio.vivid-hosting.net/" padding={12} /></a>
 			{/if}
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.roborio}>
-			<span slot="header">roboRio 2</span>
+			<span slot="header" class="font-bold">roboRio 2</span>
 			{#if openState.roborio === true || loadedState.roborio === true}
 				<a
-					href="https://www.ni.com/docs/en-US/bundle/roborio/resource/roborio.pdf?srsltid=AfmBOopSjpf-vTxFsZYLIQrGhHn-m7GKoiZHkRsGJSpym2-IW-Q_xw-x"
+					href="https://www.ni.com/docs/en-US/bundle/roborio-20-umanual/page/umanual.html"
 					target="_blank"
 					><QrCode
-						value="https://www.ni.com/docs/en-US/bundle/roborio/resource/roborio.pdf?srsltid=AfmBOopSjpf-vTxFsZYLIQrGhHn-m7GKoiZHkRsGJSpym2-IW-Q_xw-x"
+						value="https://www.ni.com/docs/en-US/bundle/roborio-20-umanual/page/umanual.html"
 						padding={12}
 					/></a
 				>
@@ -72,7 +78,7 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.sparkmax}>
-			<span slot="header">REV Spark Max</span>
+			<span slot="header" class="font-bold">REV Spark Max</span>
 			{#if openState.sparkmax === true || loadedState.sparkmax === true}
 				<a href="https://docs.revrobotics.com/brushless/spark-max/overview/" target="_blank"
 					><QrCode value="https://docs.revrobotics.com/brushless/spark-max/overview" padding={12} /></a
@@ -81,7 +87,7 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.sparkflex}>
-			<span slot="header">REV Spark Flex</span>
+			<span slot="header" class="font-bold">REV Spark Flex</span>
 			{#if openState.sparkflex === true || loadedState.sparkflex === true}
 				<a href="https://docs.revrobotics.com/brushless/spark-flex/overview" target="_blank"
 					><QrCode value="https://docs.revrobotics.com/brushless/spark-flex/overview" padding={12} /></a
@@ -90,7 +96,7 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.talonfx}>
-			<span slot="header">CTRE Talon FX</span>
+			<span slot="header" class="font-bold">CTRE Talon FX</span>
 			{#if openState.talonfx === true || loadedState.talonfx === true}
 				<a href="https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/talonfx/" target="_blank"
 					><QrCode value="https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/talonfx/" padding={12} /></a
@@ -99,7 +105,7 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.talonsrx}>
-			<span slot="header">CTRE Talon SRX</span>
+			<span slot="header" class="font-bold">CTRE Talon SRX</span>
 			{#if openState.talonsrx === true || loadedState.talonsrx === true}
 				<a href="https://ctre.download/files/user-manual/Talon%20SRX%20User's%20Guide.pdf" target="_blank"
 					><QrCode value="https://ctre.download/files/user-manual/Talon%20SRX%20User's%20Guide.pdf" padding={12} /></a
@@ -108,7 +114,7 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.victorspx}>
-			<span slot="header">CTRE Victor SPX</span>
+			<span slot="header" class="font-bold">CTRE Victor SPX</span>
 			{#if openState.victorspx === true || loadedState.victorspx === true}
 				<a href="https://ctre.download/files/user-manual/Victor%20SPX%20User's%20Guide.pdf" target="_blank"
 					><QrCode value="https://ctre.download/files/user-manual/Victor%20SPX%20User's%20Guide.pdf" padding={12} /></a
@@ -117,7 +123,7 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.canivore}>
-			<span slot="header">CTRE CANivore</span>
+			<span slot="header" class="font-bold">CTRE CANivore</span>
 			{#if openState.canivore === true || loadedState.canivore === true}
 				<a href="https://ctre.download/files/user-manual/CANivore%20User's%20Guide.pdf" target="_blank"
 					><QrCode value="https://ctre.download/files/user-manual/CANivore%20User's%20Guide.pdf" padding={12} /></a
@@ -126,10 +132,28 @@
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white" bind:open={openState.pigeon}>
-			<span slot="header">CTRE Pigeon 2.0</span>
+			<span slot="header" class="font-bold">CTRE Pigeon 2.0</span>
 			{#if openState.pigeon === true || loadedState.pigeon === true}
 				<a href="https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/pigeon2/" target="_blank"
 					><QrCode value="https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/pigeon2/" padding={12} /></a
+				>
+			{/if}
+		</AccordionItem>
+
+		<AccordionItem class="text-black dark:text-white" bind:open={openState.pdh}>
+			<span slot="header" class="font-bold">REV Power Distribution Hub</span>
+			{#if openState.pdh === true || loadedState.pdh === true}
+				<a href="https://docs.revrobotics.com/ion-control-system/pdh/overview" target="_blank"
+					><QrCode value="https://docs.revrobotics.com/ion-control-system/pdh/overview" padding={12} /></a
+				>
+			{/if}
+		</AccordionItem>
+
+		<AccordionItem class="text-black dark:text-white" bind:open={openState.cancoder}>
+			<span slot="header" class="font-bold">CTRE CANcoder</span>
+			{#if openState.cancoder === true || loadedState.cancoder === true}
+				<a href="https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/cancoder/index.html" target="_blank"
+					><QrCode value="https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/cancoder/index.html" padding={5} /></a
 				>
 			{/if}
 		</AccordionItem>
--- app/src/pages/references/WiringDiagrams.svelte
+++ app/src/pages/references/WiringDiagrams.svelte
@@ -6,12 +6,12 @@
 	<h1 class="text-3xl" style="font-weight: bold">Wiring Diagrams</h1>
 	<Accordion flush class="text-left">
 		<AccordionItem class="text-black dark:text-white">
-			<span slot="header">CTRE + POE Diagram</span>
+			<span slot="header" class="font-bold">CTRE + POE Diagram</span>
 			<img src="/app/frc-control-system-layout-ctre.svg" class="bg-white" alt="Wiring Diagram Using CTRE Components and Power Over Ethernet" />
 		</AccordionItem>
 
 		<AccordionItem class="text-black dark:text-white">
-			<span slot="header">REV + RPM Diagram</span>
+			<span slot="header" class="font-bold">REV + RPM Diagram</span>
 			<img src="/app/frc-control-system-layout-rev.svg" alt="Wiring Diagram Using REV Components and Radio Power Module" />
 		</AccordionItem>
 	</Accordion>

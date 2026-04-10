b36bfa4be81ef9a668e84e26d6988a25689c2810
Filip Kin <me@filipkin.com>
2026-04-10 01:09:29 -0400

    Fix app being broken after silent update

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -41,17 +41,6 @@
 	import { track } from "./util/telemetry";
 	import { update, VERSIONS } from "./util/updater";
 
-	window.addEventListener("error", function (event) {
-		if (
-			event?.message?.includes("Failed to fetch dynamically imported module") &&
-			!sessionStorage.getItem("reloaded")
-		) {
-			console.warn("Detected missing JS chunk. Reloading...");
-			sessionStorage.setItem("reloaded", "true");
-			window.location.reload();
-		}
-	});
-
 	// On mount check if the user's permissions have changed
 	onMount(async () => {
 		try {
